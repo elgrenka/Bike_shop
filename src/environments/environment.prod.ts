02317479c3cdd47a11291779811fee885b5cc3dd
elgrenka <bugigen@yandex.ru>
2022-05-08 22:05:04 +0400

    modify environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJt5soXPQa1-9eKKpC0_zBYmhhGkQejMg",
+    authDomain: "bike-shop-a2ebe.firebaseapp.com",
+    databaseURL: "https://bike-shop-a2ebe-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bike-shop-a2ebe",
+    storageBucket: "bike-shop-a2ebe.appspot.com",
+    messagingSenderId: "916752060130",
+    appId: "1:916752060130:web:c0df9d3999a8922c3318bb"
+  }
 };
